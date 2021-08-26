05943e1be80866ffa0a9d1ec87564569afb4317f
Andre Popovitch <andre@popovit.ch>
2021-08-26 02:09:54 +0000

    just want to get it working for 1-declaration ligo programs for now

--- src/passes/13deku-zincing/compiler.ml
+++ src/passes/13deku-zincing/compiler.ml
@@ -3,4 +3,13 @@ module AST = Ast_typed
 let compile_module : AST.module_fully_typed -> Zinc.Types.program =
  fun ast ->
   let ast = match ast with Module_Fully_Typed a -> a in
-  assert false
+  let fst = match ast |> List.hd with Some a -> a in
+  let content = fst.wrap_content in
+  let idk =
+    match content with
+    | Declaration_constant declaration_constant -> assert false
+    | Declaration_type declaration_type -> assert false
+    | Declaration_module declaration_module -> assert false
+    | Module_alias module_alias -> assert false
+  in
+  idk
