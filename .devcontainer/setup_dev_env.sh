41a52f7a45201ff3d57236d75749c3cc68186210
Andre Popovitch <andre@popovit.ch>
2021-10-01 00:38:13 +0000

    I think I've almost got it building

--- .devcontainer/setup_dev_env.sh
+++ .devcontainer/setup_dev_env.sh
@@ -1,12 +1,12 @@
-git push -fopam init --disable-sandboxing --bare -y 
+#opam init --disable-sandboxing --bare -y 
 
-echo "Setting up dev switch"
-opam update 
-./scripts/setup_dev_switch.sh 
+#echo "Setting up dev switch"
+#opam update 
+#./scripts/setup_dev_switch.sh 
 
-opam install -y --deps-only --with-test --locked=locked ./ligo.opam 
+#opam install -y --deps-only --with-test --locked=locked ./ligo.opam 
 
 
-setup_opam="eval \`opam config env\` "
-echo $setup_opam >> ~/.bashrc
-echo $setup_opam >> ~/.zshrc
+#setup_opam="eval \`opam config env\` "
+#echo $setup_opam >> ~/.bashrc
+#echo $setup_opam >> ~/.zshrc
