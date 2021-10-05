ee76d4de303baaa52067d189dd9127bdb85538a8
Andre Popovitch <andre@popovit.ch>
2021-10-05 14:01:56 +0000

    couldn't find function to get typed_ast from string

--- bin/ligo_wrapper.ml
+++ bin/ligo_wrapper.ml
@@ -1,8 +1,34 @@
-let _ =  [ Zinc.Types.Num (Z.of_int 42); Return ]
+let _ = [ Zinc.Types.Num (Z.of_int 42); Return ]
 
-let () = match Sys.argv with 
-  | [| _; "ligo-to-zinc";   y |] -> let raise = Simple_utils.Trace.{ raise = fun _ -> assert false } in 
-                                    let add_warning = fun _ -> () in 
-                                    Test_helpers.to_zinc ~raise ~add_warning y Env Test_helpers.options |> Zinc.Types.program_to_yojson |> Yojson.Safe.pretty_to_string |> print_endline;
-  | [| _; "interpret-zinc"; y |] -> print_endline y;
-  | _ -> print_endline "Hello, World!"
+(*
+
+  let typed, _ = type_file ~raise ~add_warning ~options f stx env in
+                    +- Reasonligo.preprocess_file
+
+
+  let zinc = Zinc_of_typed.compile ~raise typed in
+
+*)
+
+let ligo_to_zinc ~raise ~add_warning ligo_str =
+  Test_helpers.to_zinc ~raise ~add_warning ligo_str Env Test_helpers.options
+  |> Zinc.Types.program_to_yojson |> Yojson.Safe.pretty_to_string
+
+let interpret_zinc ~raise:_ ~add_warning:_ _zinc_str =
+  failwith "not implemented yet"
+
+let main ~raise ~add_warning ~options:_ () =
+  let str =
+    match Sys.argv with
+    | [| _; "ligo-to-zinc"; ligo_str |] ->
+        ligo_to_zinc ~raise ~add_warning ligo_str
+    | [| _; "interpret-zinc"; y |] -> interpret_zinc ~raise ~add_warning y
+    | _ -> "Invalid input!"
+  in
+  print_endline str
+
+let () =
+  let raise = Simple_utils.Trace.{ raise = (fun _ -> assert false) } in
+  let add_warning _ = () in
+  let options = Test_helpers.options in
+  main ~raise ~add_warning ~options ()
